70ffe4f0c7b6eac9756f2aeebf3f601200608330
SerenityInCode <ms1573@exeter.ac.uk>
2024-12-23 23:26:03 +0000

    Uncommented code BasicPythonPrograms.py

--- BasicPythonPrograms.py
+++ BasicPythonPrograms.py
@@ -1,89 +1,89 @@
-#Program1
-#Input
-# age=int(input("Enter age: "))
-# #Process
-# if age>18:
-#     print("Eligible to vote")
-# else:
-#     print("Not eligible to vote")    
+# Program1
+# Input
+age=int(input("Enter age: "))
+#Process
+if age>18:
+    print("Eligible to vote")
+else:
+    print("Not eligible to vote")    
 
-# #Program2
-# #Input
-# engmarks=int(input("Enter English Marks:"))
-# hindimarks=int(input("Enter Hindi Marks: "))
-# scimarks=int(input("Enter Science Marks: "))
-# mathsmarks=int(input("Enter Maths Marks: "))
-# ssmarks=int(input("Enter Social Science Marks: "))
-# #Process
-# marks=engmarks+hindimarks+scimarks+mathsmarks+ssmarks
-# totalmarks=250
-# print("The total marks is: ",marks)
-# percentage=(marks/250)*100
-# print("The percentage is: ",percentage)
-# if percentage>90:
-#     print("Your grade is A")
-# elif percentage>80:
-#     print("Your grade is B")
-# elif percentage>70:
-#     print("Your grade is C")
-# elif percentage>50:
-#     print("Your grade is D")
-# else:
-#     print("Your grade is E")               
-
-#Program3
-#Find max between 2 numbers
+#Program2
 #Input
-# a=int(input("Enter a: "))
-# b=int(input("Enter b: " ))
-# #Process
-# if a>b:
-#     print("The max number is: ",a)
-# else:
-#     print("The max number is: ",b)
+engmarks=int(input("Enter English Marks:"))
+hindimarks=int(input("Enter Hindi Marks: "))
+scimarks=int(input("Enter Science Marks: "))
+mathsmarks=int(input("Enter Maths Marks: "))
+ssmarks=int(input("Enter Social Science Marks: "))
+#Process
+marks=engmarks+hindimarks+scimarks+mathsmarks+ssmarks
+totalmarks=250
+print("The total marks is: ",marks)
+percentage=(marks/250)*100
+print("The percentage is: ",percentage)
+if percentage>90:
+    print("Your grade is A")
+elif percentage>80:
+    print("Your grade is B")
+elif percentage>70:
+    print("Your grade is C")
+elif percentage>50:
+    print("Your grade is D")
+else:
+    print("Your grade is E")               
 
-#Program4
-#Find max between 3 no.s
-#Input
-# a=int(input("Enter a: "))
-# b=int(input("Enter b: "))
-# c=int(input("Enter c: "))
-# #Process
-# if a>b and a>c:
-#     print("The max number is: ",a)
-# elif b>a and b>c:
-#     print("The max number is: ",b)       
-# else:
-#     print("The max number is: ",c)        
+# Program3
+# Find max between 2 numbers
+# Input
+a=int(input("Enter a: "))
+b=int(input("Enter b: " ))
+#Process
+if a>b:
+    print("The max number is: ",a)
+else:
+    print("The max number is: ",b)
 
-#Program 5
-#Check whether the number is even or odd
-#Input
-# number=int(input("Enter number: "))
-# #Process
-# if number%2==0:
-#     print("The number is even")
-# else:
-#     print("The number is odd")
+# Program4
+# Find max between 3 no.s
+# Input
+a=int(input("Enter a: "))
+b=int(input("Enter b: "))
+c=int(input("Enter c: "))
+#Process
+if a>b and a>c:
+    print("The max number is: ",a)
+elif b>a and b>c:
+    print("The max number is: ",b)       
+else:
+    print("The max number is: ",c)        
 
-#Program6
-#Check whether a number is divisible by 5 and 11 or not
-#Input
-# number=int(input("Enter number: "))
-# #Process
-# if number%5==0 and number%11==0:
-#     print("The number is divisible by 5 and 11")
-# else:
-#     print("The number is not divisible by 5 and 11")
+# Program 5
+# Check whether the number is even or odd
+# Input
+number=int(input("Enter number: "))
+#Process
+if number%2==0:
+    print("The number is even")
+else:
+    print("The number is odd")
+
+# Program6
+# Check whether a number is divisible by 5 and 11 or not
+# Input
+number=int(input("Enter number: "))
+#Process
+if number%5==0 and number%11==0:
+    print("The number is divisible by 5 and 11")
+else:
+    print("The number is not divisible by 5 and 11")
    
-#Program7
-#Whether the year is leap year or not
-# year=int(input("Enter a year: "))
-# #Process
-# if year%4==0:
-#     print("The year is a leap year")
-# else:
-#     print("The year is not a leap year")    
+# Program7
+# Whether the year is leap year or not
+year=int(input("Enter a year: "))
+#Process
+if year%4==0:
+    print("The year is a leap year")
+else:
+    print("The year is not a leap year")    
 
 
 
