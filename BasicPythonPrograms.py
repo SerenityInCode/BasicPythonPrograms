1438143073e6deaaa5a12c69997801b3a54b91ea
SerenityInCode <ms1573@exeter.ac.uk>
2024-12-24 20:03:59 +0000

    Updated  Select shape program BasicPythonPrograms.py

--- BasicPythonPrograms.py
+++ BasicPythonPrograms.py
@@ -101,19 +101,19 @@ if shape==1:
     area1=l*b
     print("Perimeter of rectangle is: ",perimeter1)
     print("Area of rectangle is: ",area1)
-    if shape==2:
+if shape==2:
       side1=(int(input("Enter side: "))) 
       perimeter2=4*side1
       area2=side1*side1
       print("Perimeter of square is: ",perimeter2)
       print("Area of rectangle is: ",area2)
-      if shape==3:
+if shape==3:
          radius=(int(input("Enter radius:")))    
          perimeter3=2*3.14*radius
          area3=3.14*radius*radius
          print("Perimeter of circle is: ",perimeter3)
          print("Area of circle is: ",area3)
-         if shape==4:
+if shape==4:
              side2=(int(input("Enter side of a rhombus: ")))
              d1=(int(input("Enter diagonal1: ")))
              d2=(int(input("Enter diagonal2: ")))
@@ -121,7 +121,7 @@ if shape==1:
              area4=(d1*d2)/2
              print("Perimeter of rhombus is: ",perimeter4)
              print("Area of rhombus is: ",area4)
-             if shape==5:
+if shape==5:
                  b=(int(input("Enter base: ")))
                  h=(int(input("Enter height: ")))
                  thirdside=(int(input("Enter third side: ")))
@@ -129,3 +129,16 @@ if shape==1:
                  area5=1/2*b*h
                  print("Perimeter of triangle is: ",perimeter5)
                  print("Area of triangle is: ",area5)
+
+    
+
+
+
+
+
+
+
+
+
+
+
